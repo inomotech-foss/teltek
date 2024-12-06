0f50d2eb416cb5b376862d37c2d478390c7c9948
Simon Berger <simon.berger@inomotech.com>
2024-12-06 11:41:43 +0100

    implement alternative calibration info format

--- teltek/cmd/_accelerometer.py
+++ teltek/cmd/_accelerometer.py
@@ -13,33 +13,76 @@ class AccelVector:
 
 # Calibrated! Last [2024-11-16 16:26:22] Qual:0.82 GROUND -> X:0.277259 Y:0.167735 Z:0.946040 SIDE -> X:-0.290085 Y:-0.924078 Z:0.248857 Pos:0
 # Not Calibrated! Last [1970-01-01 00:00:00] Qual:0.00 GROUND -> X:0.000000 Y:0.000000 Z:0.000000 SIDE -> X:0.000000 Y:0.000000 Z:0.000000 Pos:0
-_RE_CALIBRATION = re.compile(
-    r"(Not )?Calibrated! Last \[(.+?)\] Qual:(.+?) GROUND -> X:(.+?) Y:(.+?) Z:(.+?) SIDE -> X:(.+?) Y:(.+?) Z:(.+?) Pos:(.+?)"
+_RE_CALIBRATION1 = re.compile(
+    r"""
+    (?P<not_calibrated>Not\ )?Calibrated!
+    \ Last\ \[(?P<last>.+?)\]
+    \ Qual:(?P<qual>.+?)
+    \ GROUND\ ->\ X:(?P<g_x>.+?)\ Y:(?P<g_y>.+?)\ Z:(?P<g_z>.+?)
+    \ SIDE\ ->\ X:(?P<s_x>.+?)\ Y:(?P<s_y>.+?)\ Z:(?P<s_z>.+?)\ Pos:(?P<pos>.+?)
+    """,
+    re.VERBOSE,
+)
+# IMEI:963 Not Calibrated! Full? No! Last [1970-01-01 00:00:00] In [00:00] Qual:0.00 Pos:0\nG{0.000;0.000;0.000}\nR{0.000;0.000;0.000}\nQT1:0.00 QT2:0.00 QT3:0
+_RE_CALIBRATION2 = re.compile(
+    r"""
+    IMEI:\d+
+    \ (?P<not_calibrated>Not\ )?Calibrated!
+    \ Full\?\ (?P<full_yes_no>Yes|No)!
+    \ Last\ \[(?P<last>.+?)\]\ In\ \[(?P<in>.+?)\]
+    \ Qual:(?P<qual>.+?)
+    \ Pos:(?P<pos>.+?)
+    \nG{(?P<g_x>.+?);(?P<g_y>.+?);(?P<g_z>.+?)}
+    \nR{(?P<s_x>.+?);(?P<s_y>.+?);(?P<s_z>.+?)}
+    \nQT1:(?P<qt1>.+?) QT2:(?P<qt2>.+?) QT3:(?P<qt3>.+?)
+    """,
+    re.VERBOSE,
 )
 
 
 @dataclasses.dataclass(frozen=True)
 class AccelCalibrationInfo:
     calibrated: bool
+    full: bool | None
     last: datetime
     quality: float
     ground: AccelVector
     side: AccelVector
     pos: int
+    qt1: float | None
+    qt2: float | None
+    qt3: float | None
 
     @classmethod
     def parse(cls, s: str) -> Self:
-        m = _RE_CALIBRATION.match(s)
+        m = _RE_CALIBRATION1.match(s)
+        if m is None:
+            m = _RE_CALIBRATION2.match(s)
         if m is None:
             raise ValueError(f"Failed to parse calibration info: {s}")
-        calibrated = m.group(1) is None
-        ground = AccelVector(*map(float, m.group(4, 5, 6)))
-        side = AccelVector(*map(float, m.group(7, 8, 9)))
+        calibrated = m.group("not_calibrated") is None
+        ground = AccelVector(*map(float, m.group("g_x", "g_y", "g_z")))
+        side = AccelVector(*map(float, m.group("s_x", "s_y", "s_z")))
+
+        try:
+            full = m.group("full_yes_no") == "Yes"
+        except IndexError:
+            full = None
+
+        try:
+            qt1, qt2, qt3 = map(float, m.group("qt1", "qt2", "qt3"))
+        except IndexError:
+            qt1 = qt2 = qt3 = None
+
         return cls(
             calibrated=calibrated,
-            last=datetime.fromisoformat(m.group(2)),
-            quality=float(m.group(3)),
+            full=full,
+            last=datetime.fromisoformat(m.group("last")),
+            quality=float(m.group("qual")),
             ground=ground,
             side=side,
-            pos=int(m.group(10)),
+            pos=int(m.group("pos")),
+            qt1=qt1,
+            qt2=qt2,
+            qt3=qt3,
         )
--- tests/cmd/test_accelerometer.py
+++ tests/cmd/test_accelerometer.py
@@ -9,11 +9,34 @@ def test_calibrated():
     )
     assert info == AccelCalibrationInfo(
         calibrated=True,
+        full=None,
         last=datetime.fromisoformat("2024-11-16T16:26:22"),
         quality=0.82,
         ground=AccelVector(x=0.277259, y=0.167735, z=0.946040),
         side=AccelVector(x=-0.290085, y=-0.924078, z=0.248857),
         pos=0,
+        qt1=None,
+        qt2=None,
+        qt3=None,
+    )
+
+    info = AccelCalibrationInfo.parse(
+        """IMEI:071 Calibrated! Full? Yes! Last [2024-12-05 20:23:34] In [99:99] Qual:1.00 Pos:0
+G{-0.941;-0.337;-0.031}
+R{-0.371;0.912;0.161}
+QT1:0.75 QT2:0.00 QT3:0"""
+    )
+    assert info == AccelCalibrationInfo(
+        calibrated=True,
+        full=True,
+        last=datetime.fromisoformat("2024-12-05T20:23:34"),
+        quality=1.00,
+        ground=AccelVector(x=-0.941, y=-0.337, z=-0.031),
+        side=AccelVector(x=-0.371, y=0.912, z=0.161),
+        pos=0,
+        qt1=0.75,
+        qt2=0.00,
+        qt3=0,
     )
 
 
@@ -23,9 +46,32 @@ def test_not_calibrated():
     )
     assert info == AccelCalibrationInfo(
         calibrated=False,
+        full=None,
         last=datetime.fromisoformat("1970-01-01T00:00:00"),
         quality=0.00,
         ground=AccelVector(x=0.000000, y=0.000000, z=0.000000),
         side=AccelVector(x=0.000000, y=0.000000, z=0.000000),
         pos=0,
+        qt1=None,
+        qt2=None,
+        qt3=None,
+    )
+
+    info = AccelCalibrationInfo.parse(
+        """IMEI:963 Not Calibrated! Full? No! Last [1970-01-01 00:00:00] In [00:00] Qual:0.00 Pos:0
+G{0.000;0.000;0.000}
+R{0.000;0.000;0.000}
+QT1:0.00 QT2:0.00 QT3:0"""
+    )
+    assert info == AccelCalibrationInfo(
+        calibrated=False,
+        full=False,
+        last=datetime.fromisoformat("1970-01-01T00:00:00"),
+        quality=0.00,
+        ground=AccelVector(x=0.0, y=0.0, z=0.0),
+        side=AccelVector(x=0.0, y=0.0, z=0.0),
+        pos=0,
+        qt1=0.00,
+        qt2=0.00,
+        qt3=0,
     )
